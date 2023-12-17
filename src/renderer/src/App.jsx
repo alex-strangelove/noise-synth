1eeaa859a1881c7634ce2ec87598c3d84ea835ec
Oleksandr Melnychuk <olexm@no-reply.com>
2023-12-17 16:11:34 +0200

    Add Tone.js library

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,14 +1,23 @@
+// App.jsx
+import * as Tone from 'tone'
+import { useState } from 'react'
+
 const App = () => {
-  const ping = async () => {
-    // Send a message to the main process with the response asynchronously
-    window.electron.ipcRenderer.invoke('electron:doAThing', '').then((re) => {
-      console.log(re)
-    })
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const playSound = async () => {
+    await Tone.start()
+    const synth = new Tone.Synth().toDestination()
+    synth.triggerAttackRelease('C4', '8n')
+    setIsPlaying(true)
+    setTimeout(() => setIsPlaying(false), 500) // Reset button state after sound
   }
 
   return (
     <div>
-      <button onClick={ping}>Hello</button>
+      <button onClick={playSound} disabled={isPlaying}>
+        {isPlaying ? 'Playing...' : 'Play Tone'}
+      </button>
     </div>
   )
 }
